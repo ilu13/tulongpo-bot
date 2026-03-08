6163c4014c775cfbfef7f1d6c9f75349856f2ecb
ilu13 <imeelanieuy@yahoo.com>
2026-03-09 00:06:26 +0800

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,24 @@
+def main_menu():
+
+    return [
+        {
+            "content_type": "text",
+            "title": "🚓 Police",
+            "payload": "POLICE"
+        },
+        {
+            "content_type": "text",
+            "title": "🚑 Ambulance",
+            "payload": "AMBULANCE"
+        },
+        {
+            "content_type": "text",
+            "title": "🔥 Fire",
+            "payload": "FIRE"
+        },
+        {
+            "content_type": "text",
+            "title": "🧠 Mental Health",
+            "payload": "MENTAL_HEALTH"
+        }
+    ]
