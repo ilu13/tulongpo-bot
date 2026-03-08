fc882689bae66019a97af36aaf03f2c0f9282f24
ilu13 <imeelanieuy@yahoo.com>
2026-03-09 00:12:11 +0800

    Create contacts.py

---
+++ contacts.py
@@ -0,0 +1,26 @@
+CONTACTS = {
+    "police": {
+        "name": "Philippine National Police",
+        "number": "911"
+    },
+
+    "ambulance": {
+        "name": "Emergency Medical Services",
+        "number": "911"
+    },
+
+    "fire": {
+        "name": "Bureau of Fire Protection",
+        "number": "911"
+    },
+
+    "mental_health": {
+        "name": "Mental Health Crisis Hotline",
+        "number": "1553"
+    },
+
+    "women_children": {
+        "name": "DSWD Violence Against Women and Children",
+        "number": "1343"
+    }
+}
